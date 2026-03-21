992037a38c232aa1dce2e6047bf58b581b17571c
Ruuku420 <pookie@click4porn.org>
2026-03-20 22:01:02 -1000

    WIP file bridge
    quickfix filebridge

---
+++ src/file_bridge.py
@@ -0,0 +1,22 @@
+import packet_handler as Packet
+
+
+class FileBridge:
+    def packet_to_file(self, packet: Packet, path):
+        packet_bytes = packet.to_bytes()
+
+        packetID = Packet.packetHandler.class_to_id[packet.__class__]
+        packetLength = len(packet_bytes)
+        packetHeader = Packet.PacketHeader(packetID, packetLength)
+
+        packetTrailer = Packet.PacketTrailer()
+        packetTrailer.create_crc(packet_bytes)
+
+        data = Packet.encode(
+            header=packetHeader, payload=packet, trailer=packetTrailer
+        )
+
+        with open(path, "wb") as file:
+            file.write(data)
+
+    # TODO: def file_to_packet(self, path: str):
